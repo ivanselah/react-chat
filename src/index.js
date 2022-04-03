576d22ff933a33fb92a8722c8b5bb9073dee7a84
songmin <tft0720@gmail.com>
2022-04-03 21:53:58 +0900

    redux setting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './redux/reducers/rootReducer';
+
+const MIDDLEWARE = [promiseMiddleware, ReduxThunk];
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...MIDDLEWARE)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
